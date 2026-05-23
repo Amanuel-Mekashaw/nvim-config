4536d24045f679f6902020812d6c2c9b552c2605
Amanuel-Mekashaw <amanuelmekashaw29@gmail.com>
2026-05-23 11:31:47 +0300

    changed the colorizer plugin

--- lua/kickstart/plugins/colorizer.lua
+++ lua/kickstart/plugins/colorizer.lua
@@ -1,9 +1,24 @@
 return {
-  'norcalli/nvim-colorizer.lua',
+  'catgoose/nvim-colorizer.lua',
+  lazy = false,
   config = function()
     require('colorizer').setup {
-      '*',
-      css = { rgb_fn = true },
+      filetypes = { '*' },
+
+      user_default_options = {
+        RGB = true,
+        RRGGBB = true,
+        names = true,
+        RRGGBBAA = true,
+        AARRGGBB = true,
+        rgb_fn = true,
+        hsl_fn = true,
+        css = true,
+        css_fn = true,
+        tailwind = true,
+
+        mode = 'background',
+      },
     }
   end,
 }
